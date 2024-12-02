8ff3e94193618d902bc6b958640f09546d918437
Yvand <yvandev@outlook.fr>
2024-12-02 11:55:07 +0100

    set the authLevel to admin for getAccessToken

--- src/functions-definition/funcs-debug-def.ts
+++ src/functions-definition/funcs-debug-def.ts
@@ -1,5 +1,5 @@
 import { app } from "@azure/functions";
 import { getAccessToken, getWeb } from "../debug/funcs-debug-impl.js";
 
-app.http('debug-getAccessToken', { methods: ['GET'], authLevel: 'function', handler: getAccessToken, route: 'debug/getAccessToken' });
+app.http('debug-getAccessToken', { methods: ['GET'], authLevel: 'admin', handler: getAccessToken, route: 'debug/getAccessToken' });
 app.http('debug-getWeb', { methods: ['GET'], authLevel: 'function', handler: getWeb, route: 'debug/getWeb' });
